e673c9ce784162b906caac35354e68ad0687d55d
fahadbillah <fahadbillah@yahoo.com>
2013-04-25 23:01:30 +0600

    test commented ;)

--- test.php
+++ test.php
@@ -5,8 +5,8 @@
 	//$arr = $test->user_from_requisition_user(24);
 	//var_dump($arr);
 	//unset($test->user_data_temp);
-	$user_post = array_unique($test->getPost(24));
-	print_r($user_post);
+	//$user_post = array_unique($test->getPost(24));
+	//print_r($user_post);
 	//var_dump(array_unique($test->getPost(24)));
 	/*$test->getExtractedArray($test->get_all_user_location(24),'location_id');
 	$temp = substr($test->user_data_temp, 0, -1);
